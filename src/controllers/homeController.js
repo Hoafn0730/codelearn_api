909da9f087e58701ee9402b4223ee0bcb8171e01
Hoafn0730 <hoantran0730@gmail.com>
2023-11-13 12:00:24 +0700

    Render Dữ Liệu Ra View

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,12 @@
-const getHomePage = (req, res) => {
-    return res.render('home-page');
+const db = require('../models');
+
+const getHomePage = async (req, res) => {
+    try {
+        const data = await db.User.findAll({ raw: true });
+        return res.render('home-page', { data: JSON.stringify(data) });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const viewEngine = require('./config/viewEngine');
 const route = require('./routes');
+const connectDB = require('./config/connectDB');
 
 const app = express();
 const port = process.env.PORT || 6969;
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 route(app);
+connectDB();
 
 app.listen(port, () => {
     console.log(`App is running on port : http://localhost:${port}`);
